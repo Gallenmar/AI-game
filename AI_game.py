0b60fc5b149f7ee0ffdc52b2a01efba16c7ae2e0
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-04-05 00:02:32 +0300

    Uploading Girts Code for GUI

--- AI_game.py
+++ AI_game.py
@@ -41,9 +41,26 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.master.title("Mākslīgā intelekta spēle")
         self.master.maxsize(800, 600)
         self.master.geometry("800x600")
-        self.master.configure(bg="#ffffff")
 
         self.style = ttk.Style()
+        self.handle_light_mode()
+
+        self.human_number = 0
+        self.human_points = 0
+        self.computer_points = 0
+        self.bank_points = 0
+        self.current_number = 0
+        self.dzilums = 7
+
+        self.alpha_beta_human_number_input_flag = False
+        self.min_max_human_number_input_flag = False
+        self.turn_order = "human"
+
+        self.main_view()
+    
+    def handle_light_mode(self):
+        self.mode = "light"
+        self.master.configure(bg="#ffffff")
         self.style.configure(
             "Title.TLabel",
             foreground="midnight blue",
@@ -56,6 +73,13 @@ class AI_Game(metaclass=HandleViewsMeta):
             foreground="black",
             background="white",
             font=("Helvetica", 12),
+            padding=10
+        )        
+        self.style.configure(
+            "Winner.TLabel",
+            foreground="blue",
+            background="white",
+            font=("Helvetica", 16, "bold"),
             padding=10
         )
         self.style.configure(
@@ -72,18 +96,7 @@ class AI_Game(metaclass=HandleViewsMeta):
             font=("Helvetica", 16, "bold"),
             padding=10
         )
-        
-        self.human_number = 0
-        self.human_points = 0
-        self.computer_points = 0
-        self.bank_points = 0
-        self.current_number = 0
-        self.dzilums = 7
-
-        self.alpha_beta_human_number_input_flag = False
-        self.min_max_human_number_input_flag = False
-
-        self.main_view()
+        self.settings_view()
 
     def main_view(self):
         self.cleanup_game()
@@ -111,10 +124,17 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.min_max_button.place(relx=0.7, rely=0.5, anchor="center")
 
     def settings_view(self):
-        self.dark_mode_button = ttk.Button(self.master, text="Tumšais režīms", style="Main.TButton", command=self.handle_dark_mode)
-        self.dark_mode_button.place(relx=0.5, rely=0.5, anchor="center")
+        if (self.mode == "light"):
+            self.dark_mode_button = ttk.Button(self.master, text="Tumšais režīms", style="Main.TButton", command=self.handle_dark_mode)
+            self.dark_mode_button.place(relx=0.5, rely=0.5, anchor="center")
+            
+        elif (self.mode == "dark"):
+            self.dark_mode_button = ttk.Button(self.master, text="Gaišais režīms", style="Main.TButton", command=self.handle_light_mode)
+            self.dark_mode_button.place(relx=0.5, rely=0.5, anchor="center")
+            
 
     def handle_dark_mode(self):
+        self.mode = "dark"
         self.master.configure(bg="#696969")
         self.style.configure(
             "Title.TLabel",
@@ -144,23 +164,54 @@ class AI_Game(metaclass=HandleViewsMeta):
             font=("Helvetica", 16, "bold"),
             padding=10
         )
+        self.settings_view()
 
-
-    def alpha_beta_view(self):
+    def game_display_layout(self):
         self.current_number_display = tk.StringVar()
         self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
+        
+        self.current_number_label = ttk.Label(self.master, textvariable=self.current_number_display, style="Info.TLabel")
+        self.current_number_label.place(relx=0.2, rely=0.2, anchor="center")
+
+        self.human_points_display = tk.StringVar()
+        self.human_points_display.set("Cilvēka punkti: " + str(self.human_points))
+        
+        self.human_points_label = ttk.Label(self.master, textvariable=self.human_points_display, style="Info.TLabel")
+        self.human_points_label.place(relx=0.2, rely=0.4, anchor="center")
+
+        self.computer_points_display = tk.StringVar()
+        self.computer_points_display.set("Datora punkti: " + str(self.computer_points))
+        
+        self.computer_points_label = ttk.Label(self.master, textvariable=self.computer_points_display, style="Info.TLabel")
+        self.computer_points_label.place(relx=0.2, rely=0.5, anchor="center")
+
+        self.bank_points_display = tk.StringVar()
+        self.bank_points_display.set("Punkti bankā: " + str(self.bank_points))
+        
+        self.bank_points_label = ttk.Label(self.master, textvariable=self.bank_points_display, style="Info.TLabel")
+        self.bank_points_label.place(relx=0.2, rely=0.6, anchor="center")
+
+        self.turn_order_display = tk.StringVar()
+        self.turn_order_display.set("Gājies būs: " + ("cilvēkam" if self.turn_order == "human" else "datoram"))
+        
+        self.turn_order_label = ttk.Label(self.master, textvariable=self.turn_order_display, style="Info.TLabel")
+        self.turn_order_label.place(relx=0.85, rely=0.3, anchor="center")
+
+        self.winner_display = tk.StringVar()
+        self.winner_display.set("")
+        
+        self.winner_label = ttk.Label(self.master, textvariable=self.winner_display, style="Winner.TLabel")
+        self.winner_label.place(relx=0.85, rely=0.5, anchor="center")
+
 
         self.human_number_display = tk.StringVar()
         self.human_number_display.set("Izvēlētais skaitlis ir: " + str(self.human_number))
-        
-        self.current_number_label = ttk.Label(self.master, textvariable=self.current_number_display, style="Info.TLabel")
-        self.current_number_label.place(relx=0.8, rely=0.1, anchor="center")
 
-        self.human_number_input_label = ttk.Label(self.master, text="Izvēlieties skaitli no 25 līdz 40", style="Info.TLabel")
+        self.human_number_input_label = ttk.Label(self.master, text="Sākumā izvēlieties skaitli no 25 līdz 40", style="Info.TLabel")
         self.human_number_input_label.place(relx=0.5, rely=0.2, anchor="center")
 
         self.human_number_label = ttk.Label(self.master, textvariable=self.human_number_display, style="Info.TLabel")
-        self.human_number_label.place(relx=0.1, rely=0.2, anchor="center")
+        self.human_number_label.place(relx=0.2, rely=0.3, anchor="center")
 
         self.human_number_input = tk.Scale(self.master, from_=25, to=40, orient=tk.HORIZONTAL, length=300)
         self.human_number_input.place(relx=0.5, rely=0.3, anchor="center")
@@ -170,6 +221,18 @@ class AI_Game(metaclass=HandleViewsMeta):
 
         self.multiplier_input = tk.Scale(self.master, from_=2, to=4, orient=tk.HORIZONTAL, length=300)
         self.multiplier_input.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.show_turn_change_button()
+    
+    def show_turn_change_button(self):
+        self.turn_change_button = ttk.Button(self.master, text="Mainīt gājienu", style="Main.TButton", command=self.handle_turn_change)
+        self.turn_change_button.place(relx=0.85, rely=0.1, anchor="center")
+
+    def alpha_beta_view(self):
+        self.game_display_layout()
+
+        self.gamemode_title_input_label = ttk.Label(self.master, text="Alpha beta algoritms", style="Title.TLabel")
+        self.gamemode_title_input_label.place(relx=0.5, rely=0.1, anchor="center")
 
         self.user_submit_button = ttk.Button(self.master, text="Ievadīt", style="Main.TButton", command=self.handle_alpha_beta)
         self.user_submit_button.place(relx=0.5, rely=0.6, anchor="center")
@@ -179,29 +242,10 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.alpha_beta_text_box.place(relx=0.5, rely=0.8, anchor="center")
 
     def min_max_view(self):
-        self.current_number_display = tk.StringVar()
-        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
-        
-        self.human_number_display = tk.StringVar()
-        self.human_number_display.set("Izvēlētais skaitlis ir: " + str(self.human_number))
+        self.game_display_layout()
 
-        self.current_number_label = ttk.Label(self.master, textvariable=self.current_number_display, style="Info.TLabel")
-        self.current_number_label.place(relx=0.8, rely=0.1, anchor="center")
-
-        self.human_number_input_label = ttk.Label(self.master, text="Izvēlieties skaitli no 25 līdz 40", style="Info.TLabel")
-        self.human_number_input_label.place(relx=0.5, rely=0.2, anchor="center")
-
-        self.human_number_label = ttk.Label(self.master, textvariable=self.human_number_display, style="Info.TLabel")
-        self.human_number_label.place(relx=0.1, rely=0.2, anchor="center")
-
-        self.human_number_input = tk.Scale(self.master, from_=25, to=40, orient=tk.HORIZONTAL, length=300)
-        self.human_number_input.place(relx=0.5, rely=0.3, anchor="center")
-
-        self.multiplier_input_label = ttk.Label(self.master, text="Izvēlieties ar ko reizināt (2, 3 vai 4)", style="Info.TLabel")
-        self.multiplier_input_label.place(relx=0.5, rely=0.4, anchor="center")
-
-        self.multiplier_input = tk.Scale(self.master, from_=2, to=4, orient=tk.HORIZONTAL, length=300)
-        self.multiplier_input.place(relx=0.5, rely=0.5, anchor="center")
+        self.gamemode_title_input_label = ttk.Label(self.master, text="Minimax algoritms", style="Title.TLabel")
+        self.gamemode_title_input_label.place(relx=0.5, rely=0.1, anchor="center")
 
         self.user_submit_button = ttk.Button(self.master, text="Ievadīt", style="Main.TButton", command=self.handle_min_max)
         self.user_submit_button.place(relx=0.5, rely=0.6, anchor="center")
@@ -211,6 +255,9 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.min_max_text_box.place(relx=0.5, rely=0.8, anchor="center")
 
     def handle_alpha_beta(self):
+        if self.current_number == 0:
+            self.winner_display.set("")
+
         if self.alpha_beta_human_number_input_flag == False:
             self.human_number = int(self.human_number_input.get())
             self.human_number_display.set("Izvēlētais skaitlis ir: " + str(self.human_number))
@@ -220,6 +267,9 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.handle_game_logic("alpha_beta", self.alpha_beta_text_box)
 
     def handle_min_max(self):
+        if self.current_number == 0:
+            self.winner_display.set("")
+
         if self.min_max_human_number_input_flag == False:
             self.human_number = int(self.human_number_input.get())
             self.human_number_display.set("Izvēlētais skaitlis ir: " + str(self.human_number))
@@ -228,8 +278,32 @@ class AI_Game(metaclass=HandleViewsMeta):
 
         self.handle_game_logic("min_max", self.min_max_text_box)
 
+    def handle_turn_change(self):
+        if self.turn_order == "human":
+            self.turn_order = "computer"
+        else:
+            self.turn_order = "human"
+
+        self.turn_order_display.set("Gājies būs: " + ("cilvēkam" if self.turn_order == "human" else "datoram"))
+
     def handle_game_logic(self, gamemode, text_box):
-        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
+        if self.turn_order == "human":
+            self.human_turn(text_box)
+            self.turn_order = "computer"
+        else:
+            self.computer_turn(gamemode, text_box)
+            self.turn_order = "human"
+        
+        if self.current_number != 0:
+            self.turn_change_button.place_forget()
+            self.update_score_display()
+            self.log_score(text_box)
+        else:
+            self.show_turn_change_button()
+
+    def human_turn(self, text_box):
+        text_box.insert(tk.END, "\n\nCilvēka gājiens.")
+
         self.multiplier = int(self.multiplier_input.get())
         text_box.insert(tk.END, "\nCilvēks izvēlējās reizināt ar: " + str(self.multiplier))
 
@@ -244,26 +318,11 @@ class AI_Game(metaclass=HandleViewsMeta):
         if self.current_number % 10 == 0 or self.current_number % 10 == 5:
             self.bank_points += 1
 
-        text_box.insert(tk.END, "\nCilvēka punkti: " + str(self.human_points))
-        text_box.insert(tk.END, "\nDatora punkti: " + str(self.computer_points))
-        text_box.insert(tk.END, "\nBankas punkti: " + str(self.bank_points))
-        text_box.see("end")
-
-        # self.evaluation_result = evaluate_turn(self.current_number, self.human_points, self.computer_points, self.bank_points)
-        # if self.evaluation_result == -1:
-        #     text_box.insert(tk.END, "\nGājiens bija slikts.")
-        # elif self.evaluation_result == 0:
-        #     text_box.insert(tk.END, "\nGājiens bija viduvējs.")
-        # else:
-        #     text_box.insert(tk.END, "\nGājiens bija labs.")
-
-        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
         if self.current_number >= 5000:
             self.end_round(text_box, False)
-            self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
-            return
 
-        text_box.insert(tk.END, "\nDatora gājiens.")
+    def computer_turn(self, gamemode, text_box):
+        text_box.insert(tk.END, "\n\nDatora gājiens.")
         
         if gamemode == "alpha_beta":
             self.computer_multiplier = alphaBeta.AlphaBetaIzvele(self.current_number, self.human_points, self.computer_points, self.bank_points, self.dzilums)
@@ -275,7 +334,6 @@ class AI_Game(metaclass=HandleViewsMeta):
         self.current_number *= self.computer_multiplier
         text_box.insert(tk.END, "\nPašreizējais skaitlis: : " + str(self.current_number))
         
-
         if self.current_number % 2 == 0:
             self.computer_points -= 1
         else:
@@ -284,24 +342,20 @@ class AI_Game(metaclass=HandleViewsMeta):
         if self.current_number % 10 == 0 or self.current_number % 10 == 5:
             self.bank_points += 1
 
+        if self.current_number >= 5000:
+            self.end_round(text_box, True)
+
+    def update_score_display(self):
+        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
+        self.human_points_display.set("Cilvēka punkti: " + str(self.human_points))
+        self.computer_points_display.set("Datora punkti: " + str(self.computer_points))
+        self.bank_points_display.set("Punkti bankā: " + str(self.bank_points))
+        self.turn_order_display.set("Gājies būs: " + ("cilvēkam" if self.turn_order == "human" else "datoram"))
+
+    def log_score(self, text_box):
         text_box.insert(tk.END, "\nCilvēka punkti: " + str(self.human_points))
         text_box.insert(tk.END, "\nDatora punkti: " + str(self.computer_points))
         text_box.insert(tk.END, "\nBankas punkti: " + str(self.bank_points))
-        text_box.see("end")
-
-        # self.evaluation_result = evaluate_turn(self.current_number, self.human_points, self.computer_points, self.bank_points)
-        # if self.evaluation_result == -1:
-        #     text_box.insert(tk.END, "\nDatora gājiens bija slikts.")
-        # elif self.evaluation_result == 0:
-        #     text_box.insert(tk.END, "\nDatora gājiens bija viduvējs.")
-        # else:
-        #     text_box.insert(tk.END, "\nDatora gājiens bija labs.")
-
-        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
-        if self.current_number >= 5000:
-            self.end_round(text_box, True)
-            self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
-            return
         text_box.see("end")
 
     def end_round(self, text_box, computer_win):
@@ -310,22 +364,24 @@ class AI_Game(metaclass=HandleViewsMeta):
         else:
             self.human_points += self.bank_points
 
+        self.update_score_display()
         text_box.insert(tk.END, "\nBeigas!")
-        text_box.insert(tk.END, "\nCilvēka punkti: " + str(self.human_points))
-        text_box.insert(tk.END, "\nDatora punkti: " + str(self.computer_points))
+        self.log_score(text_box)
 
         if self.human_points > self.computer_points:
             text_box.insert(tk.END, "\nCilvēks uzvar!")
+            self.winner_display.set("Cilvēks uzvar!")
         elif self.human_points < self.computer_points:
             text_box.insert(tk.END, "\nDators uzvar!")
+            self.winner_display.set("Dators uzvar!")
         else:
             text_box.insert(tk.END, "\nNeizšķirts rezultāts!")
+            self.winner_display.set("Neizšķirts rezultāts!")
 
         text_box.insert(tk.END, "\n-----------------\n")
-        self.cleanup_game()
-        self.current_number_display.set("Pašreizējais skaitlis: " + str(self.current_number))
-        self.human_number_display.set("Izvēlētais skaitlis ir: " + str(self.human_number))
         text_box.see("end")
+
+        self.cleanup_game()
 
     def cleanup_game(self):
         self.human_number = 0
