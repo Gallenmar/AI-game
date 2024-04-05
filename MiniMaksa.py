9d7828a1c9d733eb456f59f69e78005ac443e85a
msmilgaLV <164138971+msmilgaLV@users.noreply.github.com>
2024-04-05 15:25:27 +0300

    nokomentēju kodu

--- MiniMaksa.py
+++ MiniMaksa.py
@@ -1,8 +1,8 @@
 import Tree
 
-// Rekursīva funkcija, kurai nodot Tree objektu, un bool mainīgo, kurš norāda vai nākamais līmenis ir max vai min līmenis
+# Rekursīva funkcija, kurai nodot Tree objektu, un bool mainīgo, kurš norāda vai nākamais līmenis ir max vai min līmenis
 def MiniMax(root, vaiLimenisIrMax):
-    // Ja virsotne ir sturpceļa/ gala virsotne, tai piešķir hieristisko vērtību, balstoties uz cilvēka un datora punktu skaita
+    # Ja virsotne ir sturpceļa/ gala virsotne, tai piešķir hieristisko vērtību, balstoties uz cilvēka un datora punktu skaita
     if(root.child1 == None and root.child2 == None and root.child3 == None):
         if(root.number >= 5000):
             if(root.turn == 'c'):
@@ -17,7 +17,7 @@ def MiniMax(root, vaiLimenisIrMax):
             root.heiristiskaVertiba = 0
     
     else:
-        // Ja virsotnes kādam pēctecim nav vēl hieristiskās vērtība, tas izsauc šo pašu funkciju MiniMax, lai to piešķirtu
+        # Ja virsotnes kādam pēctecim nav vēl hieristiskās vērtība, tas izsauc šo pašu funkciju MiniMax, lai to piešķirtu
         while(root.child1.heiristiskaVertiba == None or root.child2.heiristiskaVertiba == None or root.child3.heiristiskaVertiba == None):
             if(root.child1.heiristiskaVertiba == None):
                 root.child1 = MiniMax(root.child1, not vaiLimenisIrMax)
@@ -26,16 +26,16 @@ def MiniMax(root, vaiLimenisIrMax):
             if(root.child3.heiristiskaVertiba == None):
                 root.child3 = MiniMax(root.child3, not vaiLimenisIrMax)
 
-        // Kad visiem pēctečiem ir hieristiskās vērtības, virsotnei piešķir savu hieristisko vērtību, balstoties uz esošās virsotnes līmeņa
+        # Kad visiem pēctečiem ir hieristiskās vērtības, virsotnei piešķir savu hieristisko vērtību, balstoties uz esošās virsotnes līmeņa
         if(vaiLimenisIrMax):
             root.heiristiskaVertiba = max(root.child1.heiristiskaVertiba, root.child2.heiristiskaVertiba, root.child3.heiristiskaVertiba)
         else:
             root.heiristiskaVertiba = min(root.child1.heiristiskaVertiba, root.child2.heiristiskaVertiba, root.child3.heiristiskaVertiba)
 
-    //Atgriež Tree objektu, kuram ir noteiktas visas hieristiskās vērtības
+    #Atgriež Tree objektu, kuram ir noteiktas visas hieristiskās vērtības
     return root
 
-// Funkcija, no argumentie izveido koku un piešķir tās virsotnēm hieristiskās vērtības ar rekursīvo funkciju MiniMax
+# Funkcija, no argumentie izveido koku un piešķir tās virsotnēm hieristiskās vērtības ar rekursīvo funkciju MiniMax
 def MiniMaxIzvele(number, humanPoints, computerPoints, bankPoints, dzilums):
     root = Tree.TreeNode(number, humanPoints, computerPoints, bankPoints, 'c')
     root = Tree.tree_maker(root, dzilums)
@@ -45,7 +45,7 @@ def MiniMaxIzvele(number, humanPoints, computerPoints, bankPoints, dzilums):
     return izvele.index(min(izvele)) + 2
 
 
-// Testēšanai
+# Testēšanai
 #root = Tree.TreeNode(25, 0, 0, 0, 'c')
 #root = Tree.tree_maker(root, 6)
 #root = MiniMax(root, True)
